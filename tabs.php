ce7f1417a0228c5d0f3e2b2471755f3397f60293
AugerDE <david.erik.auger@gmail.com>
2017-04-29 18:04:16 -0500

    progress

--- tabs.php
+++ tabs.php
@@ -1,5 +1,4 @@
 <?php
-  include("../secure/connect.php");
   include("functions.php");
 
   if(isset($_POST['action'])){
@@ -15,46 +14,5 @@
         break;
       default:
     }
-  }
-
-  function getTasks($usrnm){
-    $conn = connectToDB();
-    $SQL = "SELECT * FROM Tasks WHERE userName=?";
-    $stmt = $conn->stmt_init();
-    if(!$stmt->prepare($SQL)){
-      $error = stmtErrorMessage($stmt->error);
-      $stmt->close();
-      $conn->close();
-      return $error;
-    }
-    $stmt->bind_param("s", $usrnm);
-    $stmt->execute();
-    $tasks = mysqli_stmt_get_result($stmt);
-    $table = "<table>
-                <thead>
-                  <tr>
-                    <th>Username</th>
-                    <th>Task Name</th>
-                    <th>Task Description</th>
-                    <th>Task Date</th>
-                    <th>Task Time</th>
-                    <th>Task Status</th>
-                  </tr>
-                </thead>
-                <tbody>";
-    while($row = $tasks->fetch_array(MYSQLI_NUM)){
-      $table .= "<tr>
-                  <td>$row[0]</td>
-                  <td>$row[1]</td>
-                  <td>$row[2]</td>
-                  <td>$row[3]</td>
-                  <td>$row[4]</td>
-                  <td>$row[5]</td>
-                </tr>";
-    }
-    $table .= "</tbody></table>";
-    $stmt->close();
-    $conn->close();
-    return $table;
   }
 ?>
