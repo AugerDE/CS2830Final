7cccc9f24cd268ae2982ce7e518172aa08887feb
AugerDE <david.erik.auger@gmail.com>
2017-04-30 14:54:19 -0500

    progress

--- functions.php
+++ functions.php
@@ -420,7 +420,7 @@
     foreach(new DirectoryIterator($dir) as $file){
       if($file->isFile()){
         $i++;
-        $images .= "<div class='thumbnail'><button class='btn' style='background: url('images/".$file."')'></button></div>";
+        $images .= "<div class='thumbnail'><button class='btn'><img src='images/".$file."' alt='".$file."' /></button></div>";
       }
       if($i % 3 == 0){
         $images .= "</div><div class='col-sm-2'>";
