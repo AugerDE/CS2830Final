64e1b400411c80215bf598d46a7c4f5e25d75ef9
AugerDE <david.erik.auger@gmail.com>
2017-04-29 20:02:57 -0500

    progress

--- tabs.php
+++ tabs.php
@@ -1,4 +1,12 @@
 <?php
+  session_start();
+  ob_start();
+  if(!isset($_SESSION['status'])){
+    session_unset();
+    session_destroy();
+    header('Location: index.php');
+  }
+
   include("functions.php");
 
   if(isset($_POST['action'])){
@@ -10,6 +18,7 @@
       case "notes":
         break;
       case "tasks":
+        $_SESSION['tab'] = "tasks";
         $usrnm = checkInput($_POST['usrnm']);
         echo getTasks($usrnm);
         break;
