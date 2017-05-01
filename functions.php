44eacba5be40a548b25d4a9eeabeee7d094cfd24
AugerDE <david.erik.auger@gmail.com>
2017-04-30 20:22:16 -0500

    progress

--- functions.php
+++ functions.php
@@ -430,7 +430,7 @@
                     <br />
                     <div class="input-group hidden" id="psswdForm">
                       <strong>Current Password:</strong>
-                      <input type="password" class="form-control" id="ogPass" />
+                      <input type="password" class="form-control" id="ogPass" /><br />
                       <strong>Re-Enter Password:</strong>
                       <input type="password" class="form-control" id="ogPassConf" />
                       <strong>New Password:</strong>
