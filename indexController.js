d296041f745b0919509b1b543902c5895a6cb78d
AugerDE <david.erik.auger@gmail.com>
2017-05-01 17:02:45 -0500

    progress

--- indexController.js
+++ indexController.js
@@ -7,7 +7,20 @@ $(function(){
       $("#errorMessage").html("<strong>ERROR: </strong>All Fields Required");
       $("#errorMessage").removeClass("hidden");
     }else{
-
+      $.post('indexHandler.php', {
+        action: 'login',
+        usrnm: usrnm,
+        psswd: psswd
+      },
+      function(data){
+        if(data == 1){
+          $("#errorMessage").html("<strong>SUCCESS: </strong>Logged In");
+          $("#errorMessage").removeClass("hidden error").addClass("good");
+        }else{
+          $("#errorMessage").html(data);
+          $("#errorMessage").removeClass("hidden");
+        }
+      });
     }
   });
 
--- indexHandler.php
+++ indexHandler.php
@@ -2,7 +2,7 @@
   session_start();
   ob_start();
 
-  include("functions.php");
+  include("../secure/connect.php");
 
   if(isset($_POST['action'])){
     $action = $_POST['action'];
@@ -10,7 +10,38 @@
       case "login":
         $usrnm = checkInput($_POST['usrnm']);
         $psswd = checkInput($_POST['psswd']);
+        $status = login($usrnm, $psswd);
+        echo $status;
         break;
     }
+  }
+
+  function checkInput($val){
+    $val = (isset($val) ? htmlspecialchars($val) : NULL);
+    return $val;
+  }
+
+  function login($usrnm, $psswd){
+    $conn = connectToDB();
+    $SQL = "SELECT * FROM Users WHERE userName=?";
+    $stmt = $conn->stmt_init();
+    if(!$stmt->prepare($SQL)){
+      $error = "<strong>ERROR: </strong>".$stmt->error;
+      $stmt->close();
+      $conn->close();
+      return $error;
+    }
+    $stmt->bind_param("s", $usrnm);
+    $stmt->execute();
+    $output = mysqli_stmt_get_result($stmt);
+    $row = $output->fetch_array(MYSQLI_NUM);
+    if($row[0] == $usrnm && password_verify($psswd, $row[1])){
+      $status = 1;
+    }else{
+      $status = "<strong>ERROR: </strong>Incorrect Username or Password";
+    }
+    $stmt->close();
+    $conn->close();
+    return $status;
   }
 ?>
