18352a4c78209ba4af8be34c8a80ee211257a1d4
AugerDE <david.erik.auger@gmail.com>
2017-04-30 22:14:05 -0500

    progress

--- functions.php
+++ functions.php
@@ -413,7 +413,7 @@
                       <button class="btn btn-danger hidden" id="userUpdateCanc" onclick="cancelUserUpdate('.$user.')">
                         <span class="glyphicon glyphicon-remove"></span>
                       </button>
-                      <div id="usernameError" class="hidden err-msg">
+                      <div id="usernameError" class="hidden errmsg">
                         <strong>ERROR: </strong>Invalid Username
                       </div>';
     $profile .=    "</div>
@@ -429,7 +429,7 @@
                        <button class="btn btn-danger hidden" id="emailUpdateCanc" onclick="cancelEmailUpdate('.$email.')">
                          <span class="glyphicon glyphicon-remove"></span>
                        </button>
-                       <div id="emailError" class="hidden err-msg">
+                       <div id="emailError" class="hidden errmsg">
                          <strong>ERROR: </strong>Invalid Email
                        </div>
                     </div>
@@ -449,16 +449,16 @@
                       </button>
                     </div>
                     <button class="btn btn-success" id="passUpdateBtn" onclick="passwordInput()">Update Password</button>
-                    <div id="emptyPassError" class="hidden err-msg">
+                    <div id="emptyPassError" class="hidden errmsg">
                       <strong>ERROR: </strong>All Inputs Required
                     </div>
-                    <div id="passMatchError" class="hidden err-msg">
+                    <div id="passMatchError" class="hidden errmsg">
                       <strong>ERROR: </strong>Passwords Do Not Match
                     </div>
-                    <div id="incorrectPass" class="hidden err-msg">
+                    <div id="incorrectPass" class="hidden errmsg">
                       <strong>ERROR: </strong>Incorrect Password
                     </div>
-                    <div id="passUpdateSuccess" class="hidden suc-msg">
+                    <div id="passUpdateSuccess" class="hidden sucmsg">
                       Password Successfully Updated
                     </div>
                   </div>
