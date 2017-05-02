3f624fed2f51580e6df284856591a902cf639cf2
AugerDE <david.erik.auger@gmail.com>
2017-05-01 22:40:42 -0500

    progress

--- Tasks/taskContent.php
+++ Tasks/taskContent.php
@@ -200,6 +200,23 @@
               <button class='btn btn-success' id='addTaskBtn' onclick='addForm()'>
                 <span class='glyphicon glyphicon-plus'></span> Add a Task
               </button>
+            </div>
+            <div class='form-inline hidden' id='addTaskForm'>
+              <th><input type='text' class='form-control' id='tskname' placeholder='Task Name'/></th>
+              <th><input type='text' class='form-control' id='tskdesc' placeholder='Task Description'/></th>
+              <th><input type='text' class='form-control' id='tskdate' placeholder='Task Date'/></th>
+              <th><input type='text' class='form-control' id='tsktime' placeholder='Task Time'/></th>
+              <th><input type='text' class='form-control' id='tskstat' placeholder='Task Status'/></th>
+              <th>
+                <button class='btn btn-success' onclick='addTask()'>
+                  <span class='glyphicon glyphicon-plus'></span>
+                </button>
+              </th>
+              <th>
+                <button class='btn btn-danger' onclick='cancel()'>
+                  <span class='glyphicon glyphicon-remove'></span>
+                </button>
+              </th>
             </div>";
   }
 
