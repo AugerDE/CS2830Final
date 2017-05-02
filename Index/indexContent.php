2648def7a124cb04b595cf119260e4ba8e2b62c9
AugerDE <david.erik.auger@gmail.com>
2017-05-02 18:27:56 -0500

    progress

--- Index/indexContent.php
+++ Index/indexContent.php
@@ -3,58 +3,62 @@
     return "<div class='row'>
 
               <div class='col-sm-6'>
-                <div class='panel-heading'>Log-In</div>
-                <div class='panel-body'>
-                  <div>
-                    <div class='input-group'>
-                      <span class='input-group-addon'><i class='glyphicon glyphicon-user'></i></span>
-                      <input type='text' class='form-control' id='usrnm' placeholder='Username' />
-                    </div>
-                    <br />
-                    <div class='input-group'>
-                      <span class='input-group-addon'><i class='glyphicon glyphicon-lock'></i></span>
-                      <input type='password' class='form-control' id='psswd' placeholder='Placeholder' />
-                    </div>
-                    <br />
-                    <div class='input-group'>
-                      <button class='btn btn-success' onclick='login()'>Log-In</button>
+                <div class='panel-default'>
+                  <div class='panel-heading'>Log-In</div>
+                  <div class='panel-body'>
+                    <div>
+                      <div class='input-group'>
+                        <span class='input-group-addon'><i class='glyphicon glyphicon-user'></i></span>
+                        <input type='text' class='form-control' id='usrnm' placeholder='Username' />
+                      </div>
+                      <br />
+                      <div class='input-group'>
+                        <span class='input-group-addon'><i class='glyphicon glyphicon-lock'></i></span>
+                        <input type='password' class='form-control' id='psswd' placeholder='Placeholder' />
+                      </div>
+                      <br />
+                      <div class='input-group'>
+                        <button class='btn btn-success' onclick='login()'>Log-In</button>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class='panel-footer hidden' id='loginMessage'>
+                  <div class='panel-footer hidden' id='loginMessage'>
+                  </div>
                 </div>
               </div>
 
               <div class='col-sm-6'>
-                <div class='panel-heading'>Register</div>
-                <div class='panel-body'>
-                  <div>
-                    <div class='input-group'>
-                      <span class='input-group-addon'><i class='glyphicon glyphicon-envelope'></i></span>
-                      <input type='text' class='form-control' id='email' placeholder='Email' />
-                    </div>
-                    <br />
-                    <div class='input-group'>
-                      <span class='input-group-addon'><i class='glyphicon glyphicon-user'></i></span>
-                      <input type='text' class='form-control' id='newUser' placeholder='Username' />
-                    </div>
-                    <br />
-                    <div class='input-group'>
-                      <span class='input-group-addon'><i class='glyphicon glyphicon-lock'></i></span>
-                      <input type='password' class='form-control' id='newPsswd' placeholder='Password' />
-                    </div>
-                    <br />
-                    <div class='input-group'>
-                      <span class='input-group-addon'><i class='glyphicon glyphicon-lock'></i></span>
-                      <input type='password' class='form-control' id='psswdConf' placeholder='Confirm Password' />
-                    </div>
-                    <br />
-                    <div class='input-group'>
-                      <button class='btn btn-success' onclick='register()'>Log-In</button>
+                <div class='panel-default'>
+                  <div class='panel-heading'>Register</div>
+                  <div class='panel-body'>
+                    <div>
+                      <div class='input-group'>
+                        <span class='input-group-addon'><i class='glyphicon glyphicon-envelope'></i></span>
+                        <input type='text' class='form-control' id='email' placeholder='Email' />
+                      </div>
+                      <br />
+                      <div class='input-group'>
+                        <span class='input-group-addon'><i class='glyphicon glyphicon-user'></i></span>
+                        <input type='text' class='form-control' id='newUser' placeholder='Username' />
+                      </div>
+                      <br />
+                      <div class='input-group'>
+                        <span class='input-group-addon'><i class='glyphicon glyphicon-lock'></i></span>
+                        <input type='password' class='form-control' id='newPsswd' placeholder='Password' />
+                      </div>
+                      <br />
+                      <div class='input-group'>
+                        <span class='input-group-addon'><i class='glyphicon glyphicon-lock'></i></span>
+                        <input type='password' class='form-control' id='psswdConf' placeholder='Confirm Password' />
+                      </div>
+                      <br />
+                      <div class='input-group'>
+                        <button class='btn btn-success' onclick='register()'>Log-In</button>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class='panel-footer hidden' id='registerMessage'>
+                  <div class='panel-footer hidden' id='registerMessage'>
+                  </div>
                 </div>
               </div>
 
