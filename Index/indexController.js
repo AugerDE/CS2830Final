ab97c030f843f7cef2e3af178bf97ee2d0ebbba6
AugerDE <david.erik.auger@gmail.com>
2017-05-02 19:25:33 -0500

    progress

--- Index/indexController.js
+++ Index/indexController.js
@@ -14,26 +14,20 @@ function load(){
 function login(){
   var usrnm = $("#usrnm").val();
   var psswd = $("#psswd").val();
-  if(usrnm == "" || psswd == ""){
-    $("#loginMessage").html("<strong>ERROR: </strong>All Inputs Required");
-    $("#loginMessage").addClass("error");
-    $("#loginMessage").removeClass("hidden");
-  }else{
-    $.post('Index/indexHandler.php', {
-      action: 'login',
-      usrnm: usrnm,
-      psswd: psswd
-    },
-    function(data){
-      if(data == 1){
-        window.location.href = "home.php";
-      }else{
-        $("#loginMessage").html(data);
-        $("#loginMessage").addClass("error");
-        $("#loginMessage").removeClass("hidden");
-      }
-    });
-  }
+  $.post('Index/indexHandler.php', {
+    action: 'login',
+    usrnm: usrnm,
+    psswd: psswd
+  },
+  function(data){
+    if(data == 1){
+      window.location.href = "home.php";
+    }else{
+      $("#loginMessage").html(data);
+      $("#loginMessage").addClass("error");
+      $("#loginMessage").removeClass("hidden");
+    }
+  });
 }
 
 function register(){
