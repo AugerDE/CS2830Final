dd66e5a814452ae105e41d6554960a24de3bfc5e
AugerDE <david.erik.auger@gmail.com>
2017-05-03 18:21:09 -0500

    progress

--- Notes/noteContent.php
+++ Notes/noteContent.php
@@ -8,7 +8,7 @@
 
   function getNotes(){
     return "<div class='notes'>
-              <input class='noteTitle' type='text' spellcheck='false' /><button class='btn btn-sm btn-danger'></button>
+              <input class='noteTitle' type='text' spellcheck='false' /><button class='closeNote' class='btn btn-sm btn-danger'></button>
               <textarea spellcheck='false'></textarea>
             </div>
             <div class='notes'>
