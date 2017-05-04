913801b83f7af9c48da94be4de50c676599d1424
AugerDE <david.erik.auger@gmail.com>
2017-05-03 21:47:09 -0500

    progress

--- Notes/noteController.js
+++ Notes/noteController.js
@@ -57,11 +57,11 @@ function saveNotes(){
   text = [];
   var i = 0;
   $("textarea").each(function(cont){
-    text[i] = $("textarea")[i];
+    text[i] = $("textarea").html();
     i++;
   });
 
   for(i = 0; i < text.length; i++){
-    console.log(text[i].innerHTML);
+    console.log(text[i]);
   }
 }
