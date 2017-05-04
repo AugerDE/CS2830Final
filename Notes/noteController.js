c3ffc5902201cbfae450c2ea1a213a897db2d3f0
AugerDE <david.erik.auger@gmail.com>
2017-05-04 14:54:15 -0500

    progress

--- Notes/noteController.js
+++ Notes/noteController.js
@@ -61,8 +61,10 @@ function saveNotes(){
     newText = text.eq(i).val();
     oldText = text.eq(i).html();
     pos = $(".notes");
-    console.log(pos.eq(i));
-    y = 0 + pos.eq(i).position().top;
+    y = pos.eq(i).position().top;
+    x = pos.eq(i).position().left;
+    console.log(y);
+    console.log(x);
     // $.post('Notes/noteHandler.php', {
     //   action: 'save',
     //   oldText: oldText,
