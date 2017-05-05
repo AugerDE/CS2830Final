0b031c669b7a490c21f8891fc4bd44892af8f41c
AugerDE <david.erik.auger@gmail.com>
2017-05-04 21:34:39 -0500

    progress

--- Videos/videoHandler.php
+++ Videos/videoHandler.php
@@ -14,7 +14,7 @@
     $action = $_GET['action'];
     switch($action){
       case "load":
-        echo "<img src='https://img.youtube.com/vi/3t1PQJmM8P4/hqdefault.jpg' />
+        echo "<img src='https://img.youtube.com/vi/3t1PQJmM8P4/3.jpg' />
               <iframe width='420' height='315' src='https://www.youtube.com/embed/3t1PQJmM8P4' frameborder='0' allowfullscreen></iframe>";
         break;
     }
