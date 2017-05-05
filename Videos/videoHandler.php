67e0c443c30a71758d2eb556092e2a1750115d7c
AugerDE <david.erik.auger@gmail.com>
2017-05-04 21:33:40 -0500

    progress

--- Videos/videoHandler.php
+++ Videos/videoHandler.php
@@ -10,8 +10,8 @@
 
   $usrnm = (isset($_SESSION['user']) ? $_SESSION['user'] : NULL);
 
-  if(isset($_POST['action'])){
-    $action = $_POST['action'];
+  if(isset($_GET['action'])){
+    $action = $_GET['action'];
     switch($action){
       case "load":
         echo "<img src='https://img.youtube.com/vi/3t1PQJmM8P4/hqdefault.jpg' />
