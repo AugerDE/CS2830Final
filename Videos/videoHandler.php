f7188f8d945d616d18059028df4625b6bdd67903
AugerDE <david.erik.auger@gmail.com>
2017-05-04 19:15:04 -0500

    progress

--- Videos/videoHandler.php
+++ Videos/videoHandler.php
@@ -14,7 +14,7 @@
     $action = $_GET['action'];
     switch($action){
       case "load":
-        return "<iframe src='https://www.youtube.com/watch?v=3t1PQJmM8P4'></iframe>";
+        echo "<iframe src='https://www.youtube.com/watch?v=3t1PQJmM8P4'></iframe>";
         break;
     }
   }
